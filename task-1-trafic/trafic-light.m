cdd02ad7f7bc2d25289688c18992e46f908499a9
karim muhammed <karim.muhammed.said@gmail.com>
2022-01-08 16:36:47 +0200

    Add my first simple Matlab project (Trafic light)

---
+++ task-1-trafic/trafic-light.m
@@ -0,0 +1,11 @@
+%plot trafic light 
+rectangle('Position',[3.7 0 0.6 3],'FaceColor',[0 0 0]);
+rectangle('Position',[3 2.8 2 4.2],'Curvature',1,'FaceColor',[0 0 0]);
+pos1 = [3.5 3.25 1 1];
+pos2 = [3.5 4.5 1 1];
+pos3 = [3.5 5.75 1 1];
+rectangle('Position',pos1,'Curvature',[1 1],'FaceColor',[0 1 0]);
+rectangle('Position',pos2,'Curvature',[1 1],'FaceColor',[1 1 0]);
+rectangle('Position',pos3,'Curvature',[1 1],'FaceColor',[1 0 0]);
+axis equal;
+
